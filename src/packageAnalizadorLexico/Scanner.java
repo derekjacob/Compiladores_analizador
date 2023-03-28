4989c8e69e03070696b34001196fa99994852439
Angel Aguilar <55730232+AngelAguilah@users.noreply.github.com>
2023-03-28 00:44:20 -0600

    Añadido HashMap para los simbolos en Scanner.java
    Se añadió un HashMap para los simbolos.

--- src/packageAnalizadorLexico/Scanner.java
+++ src/packageAnalizadorLexico/Scanner.java
@@ -14,9 +14,10 @@ public class Scanner
     private int linea = 1;
 
     private static final Map<String, TipoToken> palabrasReservadas;
+    private static final Map<String, TipoToken> simbolos;
     static {
+        // HashMap para palabras reservadas
         palabrasReservadas = new HashMap<>();
-        palabrasReservadas.put("clase", TipoToken.CLASE);
         palabrasReservadas.put("and", TipoToken.AND);
         palabrasReservadas.put("class", TipoToken.CLASS);
         palabrasReservadas.put("else", TipoToken.ELSE );
@@ -33,6 +34,26 @@ public class Scanner
         palabrasReservadas.put("true", TipoToken.TRUE );
         palabrasReservadas.put("var", TipoToken.VAR ); //definir variables
         palabrasReservadas.put("while", TipoToken.WHILE );
+        // HashMap para simbolos
+        simbolos.put("(", TipoToken.PARENTESIS_ABRE );
+        simbolos.put(")", TipoToken.PARENTESIS_CIERRA );
+        simbolos.put("{", TipoToken.LLAVE_ABRE );
+        simbolos.put("}", TipoToken.LLAVE_CIERRA );
+        simbolos.put(",", TipoToken.COMA );
+        simbolos.put(".", TipoToken.PUNTO );
+        simbolos.put(";", TipoToken.PUNTOYCOMA );
+        simbolos.put("-", TipoToken.MENOS );
+        simbolos.put("+", TipoToken.MAS );
+        simbolos.put("*", TipoToken.ASTERISCO );
+        simbolos.put("/", TipoToken.DIAGONAL );
+        simbolos.put("!", TipoToken.ADMIRACION );
+        simbolos.put("!=", TipoToken.DIFERENTE );
+        simbolos.put("=", TipoToken.ASIGNACION );
+        simbolos.put("==", TipoToken.IGUAL );
+        simbolos.put("<", TipoToken.MENOR );
+        simbolos.put("<=", TipoToken.MENORIGUAL );
+        simbolos.put(">", TipoToken.MAYOR );
+        simbolos.put(">=", TipoToken.MAYORIGUAL );
     }
 
     Scanner(String source){
